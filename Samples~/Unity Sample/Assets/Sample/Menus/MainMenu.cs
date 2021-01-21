4c41a2e923e6cb9e703a4e2cb8ee35d46b11cb13
benjaminhate <benjamin.hate.09@gmail.com>
2021-01-21 10:11:18 +0100

    reworked structure + add Sample

---
+++ Samples~/Unity Sample/Assets/Sample/Menus/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using SimpleMenuController.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sample.Menus
+{
+    public class MainMenu : Menu
+    {
+        [Header("Buttons")]
+        public Button subMenuButton;
+        public Button baseMenuButton;
+        public Button quitButton;
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+        
+        public override void Open(Menu previousMenu)
+        {
+            base.Open(previousMenu);
+
+            ShowButtons(false);
+            StartCoroutine(ShowButtonsDelayed());
+        }
+
+        private IEnumerator ShowButtonsDelayed()
+        {
+            yield return new WaitForSeconds(1f);
+            ShowButtons(true);
+        }
+
+        private void ShowButtons(bool show)
+        {
+            subMenuButton.gameObject.SetActive(show);
+            baseMenuButton.gameObject.SetActive(show);
+            quitButton.gameObject.SetActive(show);
+        }
+    }
+}
